cc695e8e9e0092ecc2226936af2681c57e42991b
prathusha <vagdeviketha@gmail>
2023-03-10 14:31:31 +0530

    variable operator range

---
+++ operators.py
@@ -0,0 +1,74 @@
+#operators
+'''x=20
+y=5
+print(x+y)
+print(x-y)
+print(x*y)
+print(x%y)
+print(x/y)
+print(x//y)
+print(x**y)'''
+
+#Assigment operators
+'''
+x=100
+x+=5
+print(x)
+x1=x+10
+print(x1)
+x-=5
+print(x)
+x*=2
+print(x)
+x**=5
+print(x) 
+'''
+
+#comparison operator
+'''
+username ='vag'
+print(username =='vag')
+username ='vag'
+print(username=='devi')
+x=100
+y=100
+z=300
+print(x==y)
+print(x==z)
+print(x!=y)
+print(x>z)
+print(z>y)
+print(x>=y)
+'''
+
+
+#logical operators
+'''
+print(4<5 and 20>5)
+print(4>5 or 20>5)
+'''
+
+#Identity operator
+'''
+print(100 is 100)
+username='abc'
+print(username is 'xyz')
+print(username is not 'xyz')
+'''
+
+#membership operator
+
+x='a' in 'vagdevi'
+print(x)
+x1='z' in 'vagdevi'
+print(x1)
+x1='z' not in 'vagdevi'
+print(x1)
+
+
+
+
+
+
+
+
---
+++ variable.py
@@ -0,0 +1,37 @@
+#variable
+
+'''a=100
+print(a)
+b=100
+print(b)
+name='vagdevi'
+c=200
+print(a,b,c,name)
+print(id(a))
+print(id(b))
+print(id(name))
+x,y,z=100,500,400
+print(x)
+x=y=z=q=999
+print(x) '''
+
+#input and output
+
+value=input("enter a value:")
+print(value)
+print(type(value))
+value1=input('enter a value1:')
+value2=input('enter a value2:')
+print(value1)
+print(value2)
+print(value+value1)
+print(type(value+value1))
+value3=int(input('enter a value3:'))
+value4=int(input('enter a value4:'))
+print(value3+value4)
+print(value3-value4)
+print(type(value3))
+value5=float(input('enter a value5:'))
+value6=float(input('enter a value6:'))
+print(value5+value6)
+print(type(value6))
